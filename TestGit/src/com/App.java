4e15048786ee6bb0396d1344c899afbfef713e8f
shinkaryuk <Nord1disc@ukr.net>
2018-04-10 11:34:50 +0300

    Second change

--- TestGit/src/com/App.java
+++ TestGit/src/com/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Project is created");
 		System.out.println("First change");
+		System.out.println("Second change");
 
 	}
 
