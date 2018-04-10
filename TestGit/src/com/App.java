b0f39a13dd395d4350e46b0a19128e48989fdcd4
shinkaryuk <Nord1disc@ukr.net>
2018-04-10 12:55:09 +0300

    Third branch created

--- TestGit/src/com/App.java
+++ TestGit/src/com/App.java
@@ -4,8 +4,11 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Project is created");
-		System.out.println("1 change");
-		System.out.println("2 change");
+		System.out.println("1 CHANGE");
+		System.out.println("2 CHANGE");
+		System.out.println("--------------");
+		System.out.println("Second branch");
+		System.out.println("Third branch");
 
 	}
 
